0ef4268e7da6b92934c1f3ff133b3f0159ddc7d3
Sam Forrester <sam_forrester@hotmail.co.uk>
2019-11-20 20:42:17 +0000

    Updated the blackjack file so that it contains use of functions

--- blackjack.js
+++ blackjack.js
@@ -5,15 +5,29 @@ let values = ["Ace", "King", "Queen", "Jack",
 "Seven", "Six", "Five",
 "Four", "Three", "Two"];
 
-let deck = [];
-
-for (let suitIdx=0; suitIdx < suits.length; suitIdx++) {
-
-  for (let valueIdx=0; valueIdx < values.length; valueIdx++) {
-    deck.push(values[valueIdx] + " of " + suits[suitIdx]);
+function createDeck() {
+  let deck = [];
+  for (let suitIdx=0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx=0; valueIdx < values.length; valueIdx++) {
+      deck.push(values[valueIdx] + " of " + suits[suitIdx]);
+    }
   }
+  return deck;
 }
+
+function getNextCard() {
+  return deck.shift();
+}
+
+let deck=createDeck();
 
 for (let i=0; i<deck.length; i++){
   console.log(deck[i]);
 }
+
+let playerCards = [getNextCard(), getNextCard()];
+
+console.log("Welcome to Blackjack!");
+console.log("You are dealt: ");
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
